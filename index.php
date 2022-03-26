f7867c92c790361795d8674941cff2c682d6fe4a
Michal582 <46369132+Michal582@users.noreply.github.com>
2022-03-26 12:50:15 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Rent a car">
+    <meta name="author" content="Mateusz Klimczak, Michal Latoszynski">
+    <title>ZigRent</title>
+  </head>
+  <body>
+
+  </body>
+</html>
