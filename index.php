955f2d02d7a161357f657bd54a34d54056e13cbb
Michal582 <46369132+Michal582@users.noreply.github.com>
2022-03-26 12:57:58 +0100

    Add materialize css framework

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="utf-8">
     <meta name="description" content="Rent a car">
     <meta name="author" content="Mateusz Klimczak, Michal Latoszynski">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>ZigRent</title>
   </head>
   <body>
+    <nav>
 
+    </nav>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
   </body>
 </html>
