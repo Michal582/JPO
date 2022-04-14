807fe4c2d7dcb93c2fef57d2e396cfd323c18aae
torxnado <36035343+torxnado@users.noreply.github.com>
2022-04-14 21:11:49 +0200

    test

--- index.php
+++ index.php
@@ -9,29 +9,31 @@
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/master.css">
     <script src="./js/bootstrap.bundle.min.js" charset="utf-8"></script>
+    <script src="./js/script.js" charset="utf-8"></script>
+
   </head>
   <body>
     <nav class="navbar navbar-light navbar-expand-md fixed-top">
       <div class="container-fluid row">
-          <div class="col">
-            <a href="#" class="navbar-brand">ZigRent</a>
+        <div class="col">
+          <a href="#" class="navbar-brand">ZigRent</a>
+        </div>
+        <div class="col">
+          <div class="collapse navbar-collapse justify-content-center" id="navbar">
+            <ul class="navbar-nav float-start">
+              <li class="nav-item"><a class="nav-link" href="#">Rezerwuj auto</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">O nas</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Nasza flota</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
+            </ul>
           </div>
-          <div class="col">
-            <div class="collapse navbar-collapse justify-content-center" id="navbar">
-                  <ul class="navbar-nav float-start">
-                    <li class="nav-item"><a class="nav-link" href="#">Rezerwuj auto</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">O nas</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Nasza flota</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
-                  </ul>
-            </div>
-          </div>
-          <div class="col">
-            <button class="navbar-toggler float-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <button type="button" class="btn btn-danger float-end" name="button">Zaloguj się</button>
-          </div>
+        </div>
+        <div class="col">
+          <button class="navbar-toggler float-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <button type="button" class="btn btn-danger float-end" name="loginBtn">Zaloguj się</button>
+        </div>
       </div>
     </nav>
     <header>
