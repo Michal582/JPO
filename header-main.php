7e82ed3234dcc51b26f924a626aed44161012b67
Michal582 <46369132+Michal582@users.noreply.github.com>
2022-04-15 20:24:54 +0200

    a kto robi dobrze

--- header-main.php
+++ header-main.php
@@ -13,17 +13,17 @@
   </section>
 </header>
 
-<div class="modal fade" id="loginModal" data-bs-keyboard="false" data-bs-backdrop="static" role="dialog" aria-labelledby="loginModalLabel">
+<!-- <div class="modal fade" id="loginModal" data-bs-keyboard="false" data-bs-backdrop="static" role="dialog" aria-labelledby="loginModalLabel">
   <div class="modal-dialog row d-flex justify-content-center align-items-center h-100">
     <div class="modal-content bg-dark text-white">
       <div class='modal-body mx-3'>
         <div class="mb-md-5 mt-md-4 text-center">
 
-          <!-- header -->
+           header
           <h2 class="fw-bold mb-2 text-uppercase">uwaga policja</h2><br>
           <p class="text-white-50 mb-5">wyskakuj z pasów frajerze</p>
 
-          <!-- middle -->
+           middle
           <form action="" method="post">
             <div class="form-outline form-white mb-4">
               <input type="text" name="username" id="username" class="form-control form-control-lg" placeholder="numer jenca" />
@@ -33,12 +33,39 @@
               <input type="password" name="passwd" id="passwd" class="form-control form-control-lg" placeholder="potwierdzenie hasla" />
             </div>
 
-            <!-- footer -->
+             footer
             <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="reg_form.php">skleroza?</a></p>
             <input name='login_sub' class="btn btn-outline-light btn-lg px-5" type="submit" value='wrzuc mnie'>
             <label for="cls-modal">kys me</label><button type="button" id="cls-modal" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
           </form>
         </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="loginModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Zaloguj się</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="" method="post">
+          <div class="mb-3">
+            <label class="form-label">Wprowadź login</label>
+            <input type="text" name="username" id="username" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Wprowadź hasło</label>
+            <input type="password" name="passwd" id="passwd" class="form-control">
+          </div>
+          <p class="small mb-3 text-center"><a class="text-black" href="reg_form.php">Przypomnij hasło</a></p>
+          <p class="mb-3 text-center"><a class="text-black" href="" >Nie masz konta? Zarejestruj się</a></p>
+          <div class="modal-footer">
+            <input name='login_sub' class="btn btn-danger" type="submit" value="Zaloguj się">
+          </div>
+        </form>
       </div>
     </div>
   </div>
--- reg_form.php
+++ reg_form.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="pl">
-  <?php 
+  <?php
 
   include('triggers.php');
   include('head.php');
-  
+
   ?>
 
   <body>
 
-    <?php include('header.php'); ?>
-          
+    <?php include('header-main.php'); ?>
+    <?php include 'navbar-main.php' ?>
+
             <!-- header -->
             <p class="text-white-50 mb-5">rejestruj sie cepie</p>
 
@@ -20,12 +21,12 @@
                 <input type="text" name="username" id="username" class="form-control form-control-lg" placeholder="numer jenca" />
                 <input type="email" name="email" id="email" class="form-control form-control-lg" placeholder="email jenca" />
               </div>
-                    
+
               <div class="form-outline form-white mb-4 w-25">
                 <input type="text" name="passwd" id="passwd" class="form-control form-control-lg" placeholder="potwierdzenie hasla" />
                 <input type="text" name="ConfirmPasswd" id="ConfirmPasswd" class="form-control form-control-lg" placeholder="powtorz to" /> <span class='text-danger invisible' id="password-matching">chuja nie pasuje</span>
               </div>
-                    
+
               <!-- footer -->
               <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="index.php">skleroza??????</a></p>
               <input name='register_sub' id='register_sub' class="btn btn-outline-dark btn-lg px-5" type="submit" value='wrzuc mnie'>
